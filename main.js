84c248ebff34edab07007cb95afafa82e3de53eb
Patrick Navarre <patrick.navarre@codeimmersives.com>
2020-11-10 10:23:24 -0500

    made-v3-active

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 
 // version 1 - A Hard-Coded Timer
 
-setTimeout(function() {
-    console.log('5 seconds have passed');
-    console.log('\u0007');
-    }, 5000);
+// setTimeout(function() {
+//     console.log('5 seconds have passed');
+//     console.log('\u0007');
+//     }, 5000);
 
 
 
@@ -20,10 +20,10 @@ setTimeout(function() {
 
 // version 3 - Multiple Timers
 
-// const userInputs = process.argv.slice(2);
-// for(const userInput of userInputs) {
-//     setTimeout(function(){ 
-//         console.log(userInput + " seconds have passed");
-//         console.log('\u0007');
-//     }, userInput * 1000);
-// }
+const userInputs = process.argv.slice(2);
+for(const userInput of userInputs) {
+    setTimeout(function(){ 
+        console.log(userInput + " seconds have passed");
+        console.log('\u0007');
+    }, userInput * 1000);
+}
