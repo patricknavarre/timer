aa1b75b85d6f66de4c699bb57f34a4e9f3e362b6
Patrick Navarre <patrick.navarre@codeimmersives.com>
2020-11-10 10:21:27 -0500

    3-versions-completed

--- main.js
+++ main.js
@@ -1,2 +1,29 @@
 // version 0:
-// console.log('\u0007');
+
+// version 1 - A Hard-Coded Timer
+
+setTimeout(function() {
+    console.log('5 seconds have passed');
+    console.log('\u0007');
+    }, 5000);
+
+
+
+//  version 2 - User Input
+
+// const userInput = process.argv[2];
+//     setTimeout(function(){
+//         console.log(userInput + " seconds have passed");
+//         console.log('\u0007');
+//     }, userInput * 1000);
+
+
+// version 3 - Multiple Timers
+
+// const userInputs = process.argv.slice(2);
+// for(const userInput of userInputs) {
+//     setTimeout(function(){ 
+//         console.log(userInput + " seconds have passed");
+//         console.log('\u0007');
+//     }, userInput * 1000);
+// }
